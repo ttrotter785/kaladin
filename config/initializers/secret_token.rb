7555de22d1385c03bcf6d00661aae2110c458e20
Tyson Trotter <ttrotter@leafsoftwaresolutions.com>
2013-10-14 17:02:06 -0400

    Blasting in a big setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kaladin::Application.config.secret_token = 'e792d8571b444185a0db898649783c72fcef9d81346c0f12b54dfb780f986fe149e3a51d0c2ea225e1523c436dcb179411130f58d1958c793311ac94fa199f60'
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Kaladin::Application.routes.draw do
+  root :to => 'login#index'
+  
+  get '/auth/twitter/callback', to: 'sessions#create', as: 'callback'
+  get '/auth/failure', to: 'sessions#error', as: 'failure'
+  get '/profile', to: 'sessions#show', as: 'show'
+  delete '/signout', to: 'sessions#destroy', as: 'signout'
+end
